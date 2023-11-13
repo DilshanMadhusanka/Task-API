056a045ee8e82c4f2b6c2d179c0aca33e780e6c3
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-14 01:31:21 +0530

    create controllers

---
+++ Task API/Controllers/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc; // me class eke wada krann oni dependances tika.
+
+                                // dark wela thiyenne thama use krala nathi dependancy tika 
+
+namespace Task_API.Controllers
+{
+    [Route("api/[controller]")]    // me deka class atributes. palweni eka route attribute eka api control attribute eka 
+    [ApiController]
+    public class TaskController : ControllerBase // API walata oni wena request, responce okkom commiunicate kranne base class eken.( ControllerBase)
+    {
+        // API class walata method noramaly action method kiyanwa.
+
+
+
+        [HttpGet] // get type eke method ekak 
+        public String[] Task()
+        {
+            return new String[] { "task1", "task2", "task3" };
+        }
+    }
+}
