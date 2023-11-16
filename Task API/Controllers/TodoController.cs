de9a319860f1d2b2758b37186a1f20e1d5537453
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-16 22:34:16 +0530

    create todo list

--- Task API/Controllers/TodoController.cs
+++ Task API/Controllers/TodoController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc; // me class eke wada krann oni dependances tika.
+using Task_API.Model;
 
-                                // dark wela thiyenne thama use krala nathi dependancy tika 
+// dark wela thiyenne thama use krala nathi dependancy tika 
+
+
+/*
 
 namespace Task_API.Controllers
 {
@@ -13,57 +17,137 @@ namespace Task_API.Controllers
         /* mehema hadn na noraml. methana hadapu okkoma tika comment karmu */
 
 
-        // API class walata method noramaly action method kiyanwa.
+// API class walata method noramaly action method kiyanwa.
 
-        /*
+/*
 
-        [HttpGet] // get type eke method ekak 
+[HttpGet] // get type eke method ekak 
 
-        /* API waladi retrun kranne "IActionResult " ekak. wena ewa return kran na */
-        /*
-        public String[] Task()
-        {
-            return new String[] { "task1", "task2", "task3" } ;
-        }
-        */
-
-
-        /*
-      
-        public IActionResult Task()
-        {
-            var tasks = new String[] { "task1", "task2", "task3","task4" };
-            return Ok(tasks);
-        }
-
-        [HttpPost]
-        public IActionResult NewTasks()
-        {
-            return Ok();
-        }
+/* API waladi retrun kranne "IActionResult " ekak. wena ewa return kran na */
+/*
+public String[] Task()
+{
+    return new String[] { "task1", "task2", "task3" } ;
+}
+*/
 
 
-        [HttpPut]
-        public IActionResult UpdateTask()
-        {
-            return Ok();
-        }
+/*
+
+public IActionResult Task()
+{
+    var tasks = new String[] { "task1", "task2", "task3","task4" };
+    return Ok(tasks);
+}
+
+[HttpPost]
+public IActionResult NewTasks()
+{
+    return Ok();
+}
 
 
-        [HttpDelete]
-        public IActionResult DeleteTask()
-        {
-            var somethingWentWrong = true;
-            if (somethingWentWrong)
-            {
-                return BadRequest();
-            }
-            return Ok();
-        }
-  */
+[HttpPut]
+public IActionResult UpdateTask()
+{
+    return Ok();
+}
 
+
+[HttpDelete]
+public IActionResult DeleteTask()
+{
+    var somethingWentWrong = true;
+    if (somethingWentWrong)
+    {
+        return BadRequest();
     }
+    return Ok();
+}
 
+
+}
+
+
+
+}
+
+
+*/
+
+
+
+namespace Task_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class TodoController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetTodos()
+        {
+            var myTodo = AllTodos();
+            return Ok(myTodo);
+        }
+
+
+        // GetTodos
+
+        private List<Todo> AllTodos()
+        {
+            var todos = new List<Todo>();
+            {
+               
+
+                var todo1 = new Todo
+                {
+                    Id = 1,
+                    Title = "Get books for the school",
+                    Description = " Get some books for the school library",
+                    CreatedDate = DateTime.Now,
+                    Due = DateTime.Now.AddDays(5),
+                   Status = TodoStatus.New
+
+                };
+
+                todos.Add(todo1);
+
+
+                var todo2 = new Todo
+                {
+                    Id =2,
+                    Title = "Get vegitables",
+                    Description = " Get vegitables for the school library",
+                    CreatedDate = DateTime.Now,
+                    Due = DateTime.Now.AddDays(2),
+                    Status = TodoStatus.Completed
+
+                };
+
+                todos.Add(todo2);
+
+
+
+
+                var todo3 = new Todo
+                {
+                    Id = 3,
+                    Title = "Get water",
+                    Description = " Get water for the school library",
+                    CreatedDate = DateTime.Now,
+                    Due = DateTime.Now.AddDays(4),
+                    Status = TodoStatus.New
+
+                };
+
+                todos.Add(todo3);
+            }
+
+            return todos;
+        }
+    }
 
 
 }
