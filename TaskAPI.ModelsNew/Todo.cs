78576e8e0940dcd7f076a588194830db774dd36c
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-18 21:04:08 +0530

    Layered Architechure

---
+++ TaskAPI.ModelsNew/Todo.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_API.Model
+{
+    public class Todo
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime Due { get; set;}
+        public TodoStatus Status { get; set; } // New , Inprogress , Completed ( me tika dala class ekak hadala thiyenne , typing krankot erros enn puluwan nisa)
+    }
+    
+}
